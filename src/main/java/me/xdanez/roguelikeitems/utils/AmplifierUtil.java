ee5447d5c0435360c1c1236d7477023ab3e126fc
xDaneZ <benderbd7@gmail.com>
2024-12-26 12:49:38 +0100

    added amplifier chances to util

--- src/main/java/me/xdanez/roguelikeitems/utils/AmplifierUtil.java
+++ src/main/java/me/xdanez/roguelikeitems/utils/AmplifierUtil.java
@@ -1,12 +1,15 @@
 package me.xdanez.roguelikeitems.utils;
 
 import me.xdanez.roguelikeitems.enums.ItemType;
+import me.xdanez.roguelikeitems.models.AmplifierChance;
 import me.xdanez.roguelikeitems.models.ConfigData;
 import me.xdanez.roguelikeitems.utils.amplifiers.DamageAmplifierUtil;
 import me.xdanez.roguelikeitems.utils.amplifiers.DurabilityAmplifierUtil;
 import me.xdanez.roguelikeitems.utils.amplifiers.MaxHealthAmplifierUtil;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 final public class AmplifierUtil {
 
@@ -16,17 +19,19 @@ final public class AmplifierUtil {
         if (material.equals(Material.AIR)) return;
         if (!ItemType.isModifiable(material)) return;
         ConfigData configData = ConfigData.getConfigData();
+        AmplifierChance amplifierChance = configData.getAmplifierChance();
 
         if (configData.getIgnoreItemList().contains(item)) return;
 
-        if (configData.useMaxHealthAmplifier()) {
+        if (configData.useMaxHealthAmplifier()
+                && amplifierChance.getMaxHealth() >= ThreadLocalRandom.current().nextInt(100 + 1))
             MaxHealthAmplifierUtil.setMaxHealthData(item);
-        }
-        if (configData.useDurabilityAmplifier()) {
+
+        if (configData.useDurabilityAmplifier()
+                && amplifierChance.getDurability() >= ThreadLocalRandom.current().nextInt(100 + 1))
             DurabilityAmplifierUtil.setDurabilityData(item);
-        }
-        if (configData.useDamageAmplifier() && !ItemType.hasNoDamage(material)) {
-            DamageAmplifierUtil.setDamageAmplifierData(item);
-        }
+
+        if (configData.useDamageAmplifier() && !ItemType.hasNoDamage(material)
+                && amplifierChance.getDamage() >= ThreadLocalRandom.current().nextInt(100 + 1)) DamageAmplifierUtil.setDamageAmplifierData(item);
     }
 }
